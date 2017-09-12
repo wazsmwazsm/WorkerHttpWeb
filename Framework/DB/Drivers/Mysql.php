82d1a058d4d717349be28acee80ef831b73274f8
wazsmwazsm <18748406022@163.com>
2017-09-12 20:07:08 +0800

    update mysql drivers, add group, having , or having

--- Framework/DB/Drivers/Mysql.php
+++ Framework/DB/Drivers/Mysql.php
@@ -9,23 +9,24 @@ use PDOException;
  */
 class Mysql implements ConnectorInterface {
 
-    private $_pdo;
+    private $_pdo = NULL;
     /**
      * PDOStatement instance
      *
      * @var \PDOStatement
      */
-    private $_pdoSt;
-    private $_config;
+    private $_pdoSt = NULL;
+    private $_config = [];
 
-    private $_table;
-    private $_query_sql;
+    private $_table = '';
+    private $_query_sql = '';
 
     private $_cols_str = ' * ';
-    private $_where_str;
-    private $_orderby_str;
-
-    private $_bind_params;
+    private $_where_str = '';
+    private $_orderby_str = '';
+    private $_groupby_str = '';
+    private $_having_str = '';
+    private $_bind_params = [];
 
     public function __construct($host, $port, $user, $password, $dbname, $charset = 'utf8') {
         $this->_config = [
@@ -73,12 +74,14 @@ class Mysql implements ConnectorInterface {
         $this->_cols_str = ' * ';
         $this->_where_str = '';
         $this->_orderby_str = '';
+        $this->_groupby_str = '';
+        $this->_having_str = '';
         $this->_bind_params = [];
     }
 
     private function _buildQuery() {
         $this->_query_sql = 'SELECT '.$this->_cols_str.' FROM '.$this->_table.
-            $this->_where_str.$this->_orderby_str;
+            $this->_where_str.$this->_groupby_str.$this->_orderby_str.$this->_having_str;
     }
 
     private function _bindParams() {
@@ -89,14 +92,32 @@ class Mysql implements ConnectorInterface {
         }
     }
 
+    private static function _backquote($str) {
+        // match pattern
+        $alias_pattern = '/([a-zA-Z0-9_]+)\s+(AS|as|As)\s+([a-zA-Z0-9_]+)/';
+        $alias_replace = '`$1` AS `$3`';
+        $func_pattern = '/[a-zA-Z0-9_]+\([a-zA-Z0-9_\,\s\`\'\"\*]*\)/';
+        // alias mode
+        if(preg_match($alias_pattern, $str)) {
+            return preg_replace($alias_pattern, $alias_replace, $str);
+        }
+        // mysql fun mode
+        if(preg_match($func_pattern, $str)) {
+            return $str;
+        }
+        // field mode
+        return '`'.$str.'`';
+    }
+
     public function select() {
         $cols = func_get_args();
 
         if( ! func_num_args() || in_array('*', $cols)) {
             $this->_cols_str = ' * ';
         } else {
+            $this->_cols_str = '';
             foreach ($cols as $col) {
-                $this->_cols_str .= ' '.$col.',';
+                $this->_cols_str .= ' '.self::_backquote($col).',';
             }
             $this->_cols_str = rtrim($this->_cols_str, ',');
         }
@@ -104,13 +125,10 @@ class Mysql implements ConnectorInterface {
         return $this;
     }
 
-    public function query($sql) {
-        return $this->_pdo->query($sql);
-    }
 
     public function get() {
         $this->_buildQuery();
-        
+        var_dump($this->_query_sql);
         $this->_pdoSt = $this->_pdo->prepare($this->_query_sql);
         $this->_bindParams();
 
@@ -130,6 +148,131 @@ class Mysql implements ConnectorInterface {
         return $this->_pdoSt->fetch(PDO::FETCH_ASSOC);
     }
 
+    // 条件构造的统一操作
+    private function _condition_constructor($args_num, $params, $operator, &$construct_str) {
+
+        // params dose not conform to specification
+        if( ! $args_num || $args_num > 3) {
+            throw new PDOException("Error number of parameters");
+        }
+
+        switch ($args_num) {
+          case 1:
+              if( ! is_array($params[0])) {
+                  throw new PDOException($params[0].' should be Array');
+              }
+              foreach ($params[0] as $field => $value) {
+                  $plh = ':'.bin2hex($field.'_'.$value);
+                  $construct_str .= ' '.self::_backquote($field).' = '.$plh.' '.$operator;
+                  $this->_bind_params[$plh] = $value;
+              }
+              // 想想有没有更好的处理方案
+              $construct_str = rtrim($construct_str, $operator);
+              break;
+          case 2:
+              $plh = ':'.bin2hex($params[0].'_'.$params[1]);
+              $construct_str .= ' '.self::_backquote($params[0]).' = '.$plh.' ';
+              $this->_bind_params[$plh] = $params[1];
+              break;
+          case 3:
+              if( ! in_array($params[1], ['<', '>', '<=', '>=', '=', '!=', '<>'])) {
+                  throw new PDOException('Confusing Symbol '.$params[1]);
+              }
+              $plh = ':'.bin2hex($params[0].'_'.$params[1].'_'.$params[2]);
+              $construct_str .= ' '.self::_backquote($params[0]).' '.$params[1].' '.$plh.' ';
+              $this->_bind_params[$plh] = $params[2];
+              break;
+        }
+    }
+
+    public function where() {
+
+        $operator = 'AND';
+
+        // is the first time call where method ?
+        if($this->_where_str == '') {
+            $this->_where_str = ' WHERE ';
+        } else {
+            $this->_where_str .= ' '.$operator.' ';
+        }
+
+        $this->_condition_constructor(func_num_args(), func_get_args(), $operator, $this->_where_str);
+
+        return $this;
+    }
+
+    public function orWhere() {
+
+        $operator = 'OR';
+
+        // is the first time call where method ?
+        if($this->_where_str == '') {
+            $this->_where_str = ' WHERE ';
+        } else {
+            $this->_where_str .= ' '.$operator.' ';
+        }
+
+        $this->_condition_constructor(func_num_args(), func_get_args(), $operator, $this->_where_str);
+
+        return $this;
+    }
+
+    public function groupBy($field) {
+        // is the first time call groupBy method ?
+        if($this->_groupby_str == '') {
+            $this->_groupby_str = ' GROUP BY '.self::_backquote($field);
+        } else {
+            $this->_groupby_str .= ' , '.self::_backquote($field);
+        }
+
+        return $this;
+    }
+
+    public function having() {
+        $operator = 'AND';
+
+        // is the first time call where method ?
+        if($this->_having_str == '') {
+            $this->_having_str = ' HAVING ';
+        } else {
+            $this->_having_str .= ' '.$operator.' ';
+        }
+
+        $this->_condition_constructor(func_num_args(), func_get_args(), $operator, $this->_having_str);
+
+        return $this;
+    }
+
+    public function orHaving() {
+        $operator = 'OR';
+
+        // is the first time call where method ?
+        if($this->_having_str == '') {
+            $this->_having_str = ' HAVING ';
+        } else {
+            $this->_having_str .= ' '.$operator.' ';
+        }
+
+        $this->_condition_constructor(func_num_args(), func_get_args(), $operator, $this->_having_str);
+
+        return $this;
+    }
+
+    public function orderBy($field, $mode) {
+        // is the first time call orderBy method ?
+        if($this->_orderby_str == '') {
+            $this->_orderby_str = ' ORDER BY '.self::_backquote($field).' '.$mode;
+        } else {
+            $this->_orderby_str .= ' , '.self::_backquote($field).' '.$mode;
+        }
+
+        return $this;
+    }
+
+    public function query($sql) {
+        return $this->_pdo->query($sql);
+    }
+
     public function insert() {
 
     }
@@ -142,69 +285,4 @@ class Mysql implements ConnectorInterface {
 
     }
 
-    public function where() {
-        $args_num = func_num_args();
-        $params   = func_get_args();
-
-        // params dose not conform to specification
-        if( ! $args_num || $args_num > 3) {
-            return $this;
-        }
-
-        // is the first time call where method ?
-        if($this->_where_str == '') {
-            $this->_where_str = ' WHERE ';
-        } else {
-            $this->_where_str .= ' AND ';
-        }
-
-        switch ($args_num) {
-          case 1:
-              if( ! is_array($params[0])) {
-                  throw new PDOException($params[0].' should be Array');
-              }
-              foreach ($params[0] as $field => $value) {
-                  $plh = ':'.bin2hex($field.'_'.$value);
-                  $this->_where_str .= ' '.$field.' = '.$plh.' AND';
-                  $this->_bind_params[$plh] = $value;
-              }
-              // 想想有没有更好的处理方案
-              $this->_where_str = rtrim($this->_where_str, 'AND');
-              break;
-          case 2:
-              $plh = ':'.bin2hex($params[0].'_'.$params[1]);
-              $this->_where_str .= ' '.$params[0].' = '.$plh.' ';
-              $this->_bind_params[$plh] = $params[1];
-              break;
-          case 3:
-              if( ! in_array($params[1], ['<', '>', '<=', '>=', '=', '!=', '<>'])) {
-                  throw new PDOException('Confusing Symbol '.$params[1]);
-              }
-              $plh = ':'.bin2hex($params[0].'_'.$params[1].'_'.$params[2]);
-              $this->_where_str .= ' '.$params[0].' '.$params[1].' '.$plh.' ';
-              $this->_bind_params[$plh] = $params[2];
-              break;
-        }
-
-        return $this;
-    }
-
-    public function group() {
-
-    }
-
-    public function having() {
-
-    }
-
-    public function orderBy($field, $mode) {
-        // is the first time call orderBy method ?
-        if($this->_orderby_str == '') {
-            $this->_orderby_str = ' ORDER BY '.$field.' '.$mode;
-        } else {
-            $this->_orderby_str .= ' , '.$field.' '.$mode;
-        }
-
-        return $this;
-    }
 }
--- app/Controller/TestController.php
+++ app/Controller/TestController.php
@@ -10,11 +10,8 @@ class TestController extends Controller {
         // $rst = DB::$connection['con1']->query('select * from ad_promote_info limit 0, 30');
 
         $rst = DB::$connection['con1']->table('ad_promote_info')
-             ->where('id', '<', 10)
-             ->where('id', '>', 2)
-             ->orderBy('adId', 'asc')
-             ->orderBy('id', 'desc')
              ->get();
+             
         return $rst;
     }
 }
