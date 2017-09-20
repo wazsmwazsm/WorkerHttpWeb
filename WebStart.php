2943323d97bb690d897b709daa1e904d7f7088a8
wazsmwazsm <18748406022@163.com>
2017-09-20 16:49:09 +0800

    add workerman logfile

--- WebStart.php
+++ WebStart.php
@@ -6,6 +6,7 @@ use Framework\App;
 
 // echo error message on daemon mode
 Worker::$stdoutFile = './tmp/log/error.log';
+Worker::$logFile = './tmp/log/workerman.log';
 
 $http_worker = new Worker('http://0.0.0.0:600');
 $http_worker->count = 4;
