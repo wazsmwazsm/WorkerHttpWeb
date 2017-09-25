af2cb89a2a83d3263521c1347ea5ae4842ebd643
wazsmwazsm <18748406022@163.com>
2017-09-25 17:51:42 +0800

    update DB Model, DB add catch, connection method

--- Framework/DB/DB.php
+++ Framework/DB/DB.php
@@ -14,7 +14,7 @@ class DB
      *
      * @var Array
      */
-    public static $connection = [];
+    private static $_connections = [];
 
     /**
      * init db connections.
@@ -27,22 +27,31 @@ class DB
         $db_confs = Config::get('database.connection');
         // connect database
         foreach ($db_confs as $con_name => $db_conf) {
-            switch (strtolower($db_conf['driver'])) {
-              case 'mysql':
-                self::$connection[$con_name] = new Mysql
-                (
-                    $db_conf['host'],
-                    $db_conf['port'],
-                    $db_conf['user'],
-                    $db_conf['password'],
-                    $db_conf['dbname'],
-                    $db_conf['charset']
-                );
-                break;
-              default:
-                break;
+            try {
+                switch (strtolower($db_conf['driver'])) {
+                    case 'mysql':
+                        self::$_connections[$con_name] = new Mysql
+                        (
+                            $db_conf['host'], $db_conf['port'], $db_conf['user'], $db_conf['password'], $db_conf['dbname'], $db_conf['charset']
+                        );
+                        break;
+                    default:
+                        break;
+                }
+            } catch (\Exception $e) {
+                echo "Database connect fail, check your database config for connection '$con_name' \n".$e->getMessage()."\n";
             }
-
         }
+    }
+
+    /**
+     * get db connection.
+     *
+     * @param String $con_name
+     * @return object
+     */
+    public static function connection($con_name)
+    {
+        return self::$_connections[$con_name];
     }
 }
--- app/Controller/TestController.php
+++ app/Controller/TestController.php
@@ -10,27 +10,27 @@ class TestController extends Controller
 {
     public function test(Test $model, Requests $request)
     {
-        // $rst = DB::$connection['con1']->query('select * from ad_promote_info limit 0, 30');
+        // $rst = DB::connection('con1')->query('select * from ad_promote_info limit 0, 30');
 
 
-        // $rst = DB::$connection['con2']->table('ad_promote_collect')
+        // $rst = DB::connection('con2')->table('ad_promote_collect')
         //      ->leftJoin('ad_promote_oss', 'ad_promote_oss.adId', 'ad_promote_collect.adId')
         //      ->select('ad_promote_oss.adId')
         //      ->where('ad_promote_collect.date', 1500912000)
         //      ->get();
-        // $rst = DB::$connection['con1']->table('ad_promote_info')
+        // $rst = DB::connection('con1')->table('ad_promote_info')
         //      ->where('id', '<', 10)
         //      ->get();
       //  $rst = $model
       //         ->where('id', '<', 10)
       //         ->get();
-        // $rst = DB::$connection['con2']->table('ad_promote_collect')
+        // $rst = DB::connection('con2')->table('ad_promote_collect')
         //      ->where([
         //        'adId' => '001-001',
         //        'adStyle' => 'big_ad',
         //      ])
         //      ->get();
-        // $rst = DB::$connection['con2']->table('ad_promote_collect')
+        // $rst = DB::connection('con2')->table('ad_promote_collect')
         //      ->where('id', '<', 10)
         //      ->orWhereBrackets(function($query) {
         //         $query->where('adId', '001-001')
@@ -51,15 +51,15 @@ class TestController extends Controller
         //     ])->orderBy('id', 'DESC')
         //     ->get();
 
-        // $rst = DB::$connection['con2']->table('ad_promote_collect')
+        // $rst = DB::connection('con2')->table('ad_promote_collect')
         //       ->list('id');
         // $rst = (string) $rst;
 
-        // $rst = DB::$connection['con2']->table('ad_promote_collect')
+        // $rst = DB::connection('con2')->table('ad_promote_collect')
         //      ->whereNull('adStyle')
         //      ->whereNotNull('package_name')
         //      ->get();
-        // $rst = DB::$connection['con2']->table('ad_promote_collect')
+        // $rst = DB::connection('con2')->table('ad_promote_collect')
         //      ->whereInSub('id', function($query) {
         //           $query->table('ad_promote_info')
         //                 ->select('id')->where('id', '<', '10');
@@ -67,13 +67,19 @@ class TestController extends Controller
         //      ->orderBy('id', 'DESC')
         //      ->get();
 
-        $rst = DB::$connection['con2']->select('id','adId','adTitle')->fromSub(function($query) {
-          $query->table('ad_promote_info')->where('id', '<', '100');
-        })->where('id', '!=', 9)
-        ->orderBy('id', 'ASC')
-        ->paginate(10, $request->page);
+         $rst = $model->whereInSub('id', function($query) {
+                   $query->table('ad_promote_info')
+                         ->select('id')->where('id', '<', '100');
+              })
+              ->orderBy('id', 'DESC')
+              ->paginate(10, $request->page);
+        // $rst = DB::connection('con2')->select('id','adId','adTitle')->fromSub(function($query) {
+        //   $query->table('ad_promote_info')->where('id', '<', '100');
+        // })->where('id', '!=', 9)
+        // ->orderBy('id', 'ASC')
+        // ->paginate(10, $request->page);
 
-        // $rst = DB::$connection['con2']->table('ad_promote_collect')
+        // $rst = DB::connection('con2')->table('ad_promote_collect')
         // ->withDebug()
         // ->insert([
         //   'package_name' => 'aa',
@@ -86,13 +92,13 @@ class TestController extends Controller
         // ]);
 
 
-        // $rst = DB::$connection['con2']->table('ad_promote_collect')
+        // $rst = DB::connection('con2')->table('ad_promote_collect')
         // ->where('package_name', 'aa')->withDebug()
         // ->update([
         //   'impression' => 20,
         //   'click' => 10,
         // ]);
-        // $rst = DB::$connection['con2']->table('ad_promote_collect')
+        // $rst = DB::connection('con2')->table('ad_promote_collect')
         // ->where('package_name', 'aa')
         // ->delete();
 
