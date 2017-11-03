0a3efee7929ffd1c15ac2aef0cd2df874b5ed015
wazsmwazsm <18748406022@163.com>
2017-11-03 20:14:28 +0800

    finish group nesting

--- routes/test.php
+++ routes/test.php
@@ -14,6 +14,21 @@ Route::get('test2/', function() {
     return 'test2';
 });
 
+
+
+Route::group(['prefix' => '/g1', 'namespace' => 'App'], function() {
+    Route::group(['prefix' => '/g2', 'namespace' => 'Controller'], function() {
+        Route::get('test', function() {
+            return 'g1 g2 test success';
+        });
+        Route::get('con', "TestController@test");
+    });
+
+    Route::get('test', function() {
+        return 'g1 test success';
+    });
+});
+
 Route::group(['prefix' => '/pre', 'namespace' => 'App\Controller'], function() {
     Route::get('control/', 'TestController@test');
     Route::post('call1/', function() {
